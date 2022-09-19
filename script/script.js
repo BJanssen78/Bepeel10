126ecd7882ee5ebbdffc7d6cbdc103dc0e43f51d
BJanssen78 <98946596+BJanssen78@users.noreply.github.com>
2022-09-19 23:26:12 +0200

    adding color to button in java

--- script/script.js
+++ script/script.js
@@ -1,29 +1,39 @@
-const btns = document.querySelectorAll('#colorlist .colorlist-item');
-const box = document.getElementsByClassName('box');
-const hamburgerMenu = document.getElementById('#hamburger-menu');
-const colorlist = document.querySelector('#colorlist');
+const btns = document.querySelectorAll('#colorlist .colorlist-item-button');
+const box = document.getElementById('box');
+const txt = document.getElementById('txt');
+let homebtn = btns[0];
+let lbluebtn = btns[1];
+let greenbtn = btns[2];
+let pinkbtn = btns[3];
+let violetbtn = btns[4];
 
-hamburgerMenu.addEventListener('click', function(em){
-    console.log(em.target);
-    colorlist.classList.add('on');
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        console.log(e.target);
 
-})
-
-// Array.from(btns).forEach(function(btn){
-//     btn.addEventListener('click', function(e){
-//         const li = e.target.parentElement;
-
-//         if(className == 'lblue'){
-//             box.style.background = 'light blue';
-//         }
-//         else if(className == 'green'){
-//             box.style.background = 'green';
-//         }
-//         else if(className == 'pink'){
-//             box.style.background = 'pink';
-//         }
-//         else if(className == 'violet'){
-//             box.style.background = 'violet';
-//         }
-//     });
-// });
+        if(e.target === homebtn){
+            txt.innerHTML = ('');
+            location.reload();
+        }
+        else if(e.target === lbluebtn){
+            txt.innerHTML = ('Light Blue');
+            box.className = '';
+            box.classList.add('lblue');
+        }
+        else if(e.target === greenbtn){
+            txt.innerHTML = ('Green');
+            box.className = '';
+            box.classList.add('green');
+        }
+        else if(e.target === pinkbtn){
+            txt.innerHTML = ('Pink');
+            box.className = '';
+            box.classList.add('pink');
+        }
+        else if(e.target === violetbtn){
+            txt.innerHTML = ('Violet');
+            box.className = '';
+            box.classList.add('violet');
+        }   
+    });
+});
